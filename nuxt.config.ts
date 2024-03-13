0807b7fc45338efb7b97a3733954cc378a289825
devmini1203 <devmini1203@gmail.com>
2024-03-13 10:40:40 +0000

    chore: git actions을 위한 yml 파일 추가 및 build.sh 파일 추가 (#7)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  nitro: {
+    routeRules: {
+      // toast-ui editor 가 SSR 을 지원하지 않아 reload시 에러가 나는것을 방지하기위해 임시조치
+      "editor/toast": {
+        ssr: false,
+      },
+    },
+  },
+});
---
+++ pages/editor/toast.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+const editor = ref();
+
+const getContent = () => {
+  const content = editor.value.getHTML();
+
+  alert("콘솔을 확인해주세요");
+
+  console.log(content);
+};
+
+onMounted(() => {
+  editor.value = toastEditorInstance(
+    editor.value,
+    "wysiwyg",
+    false,
+    false,
+    "50vh"
+  );
+});
+</script>
+
+<template>
+  <div
+    style="
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      flex-direction: column;
+    "
+  >
+    <div style="display: flex; margin-bottom: 3rem; flex-direction: column">
+      <h1>toast editor</h1>
+
+      <div style="display: flex; justify-content: center">
+        <button @click="getContent">작성 내용 가져오기</button>
+      </div>
+    </div>
+
+    <div style="display: flex">
+      <div ref="editor"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
